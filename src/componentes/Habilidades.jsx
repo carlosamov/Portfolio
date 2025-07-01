68f799cacd10b456c3975d78cd6f3c51227f3638
Carlos Ovalles <carlosamov@gmail.com>
2025-07-01 16:04:18 -0400

    Improve Habilidades UI and update styles
    Enhanced the Habilidades component with visual feedback for selected navigation, updated project descriptions, and improved the carousel logic. Adjusted CSS for carousel, habilidades, and header for better layout, responsiveness, and theme consistency.

--- src/componentes/Habilidades.jsx
+++ src/componentes/Habilidades.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./estilos/habilidades.css";
 
@@ -13,18 +13,35 @@ import imgUsuarios from "../assets/Usuarios.png";
 export default function Habilidades() {
   const [page, setPage] = React.useState(1);
 
+  const selected = {
+    backgroundColor: "var(--main-secondary)",
+    color: "var(--main-text)",
+  };
+
   return (
     <section className="habilidades">
       <h2>Proyectos Destacados y Habilidades Aplicadas</h2>
       <div className="NavBar">
-        <button className="nav 1" onClick={() => setPage(1)}>
+        <button
+          style={page == 1 ? selected : undefined}
+          className="nav 1"
+          onClick={() => setPage(1)}
+        >
           Sistema de Gestión de Estudiantes
         </button>
-        <button className="nav 2" onClick={() => setPage(2)}>
+        <button
+          style={page == 2 ? selected : undefined}
+          className="nav 2"
+          onClick={() => setPage(2)}
+        >
           Sistema de Automatización de Presupuestos
         </button>
-        <button className="nav 3" onClick={() => setPage(3)}>
-          Sistema de Gestión de Estudiantes
+        <button
+          style={page == 3 ? selected : undefined}
+          className="nav 3"
+          onClick={() => setPage(3)}
+        >
+          Carrera Academica
         </button>
       </div>
       {page == 1 && <GestionEstudiantes />}
@@ -74,7 +91,7 @@ function GestionEstudiantes() {
 function AutomatizacionPresupuestos() {
   return (
     <div>
-      <h2>Automatización de Presupuestos</h2>
+      <h2>Automatización de Presupuestos (En Progreso)</h2>
       <Carousel
         images={[
           imgLogin,
@@ -85,27 +102,27 @@ function AutomatizacionPresupuestos() {
         ]}
       />
       <p>
-        Se desarrolló una aplicacion web integral para la gestion de
-        matriculación de estudiantes, junto con estadísticas de estudiantes
-        registrados a lo largo del tiempo, asi como administración de secciones.
+        Se desarrolló una aplicacion web como proyecto académico para la
+        creación de una plataforma para automatizar el proceso de elbaroación y
+        gestión de presupuestos.
       </p>
       <h3>Tecnológias Aplicadas</h3>
       <ul>
         <li>
-          <b>Frontend con React:</b> Construí la interfaz de usuario interactiva
-          y responsiva, gestionando el estado de la aplicación y la comunicación
-          con el backend.
+          <b>Frontend con React:</b> Mi principal herramienta de creación de
+          interfaces es React, domino los Hooks basicos y entiendo el ciclo de
+          vida del componente de manera sólida.
         </li>
         <li>
           <p>
-            <b>Backend con C#:</b> Diseñé en su totalidad la API RESTful usando
-            C# con EmbedIO para el servidor y EntityFramework para manipular la
-            base de datos PostgreSQL.
+            <b>Backend con Node.js:</b> Diseñé en su totalidad la API RESTful
+            usando Node.js con Express, para el servidor y Sequelize (un ORM
+            para Node.js) para manipular la base de datos PostgreSQL.
           </p>
         </li>
         <p>
-          Gracias a este proyecto aprendí a intercontectar los distintos
-          elementos de una apliación, siguiendo los patrones de diseño MVC.
+          Gracias a este proyecto aprendí a crear una interacción más fluida
+          entre componentes y afinar los conocimientos ya presentes de React.
         </p>
       </ul>
     </div>
@@ -120,11 +137,11 @@ function Carousel(props) {
   const [index, setIndex] = React.useState(0);
 
   const next = () => {
-    setIndex((index + 1) % props.images.length);
+    setIndex((prev) => (prev + 1) % props.images.length);
   };
 
   const prev = () => {
-    setIndex((index - 1 + props.images.length) % props.images.length);
+    setIndex((prev) => (prev - 1 + props.images.length) % props.images.length);
   };
 
   return (
